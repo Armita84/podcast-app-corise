a8c7f10e91c8ea1f78fbf95cda49693f04be1b03
Armita Raz <armita_white2003@yahoo.com>
2023-08-22 10:13:06 +0330

    Updated

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -32,17 +32,6 @@ def main():
     st.sidebar.subheader("Available Podcasts Feeds")
     selected_podcast = st.sidebar.selectbox("Select Podcast", options=available_podcast_info.keys())
 
-    # User Input box
-    st.sidebar.subheader("Add and Process New Podcast Feed")
-    st.sidebar.markdown(
-                f"<p style='margin-bottom: 5px; color:#000; font-style:italic; font-weight: bold;'>Don't Use RSS feed. Use the castbox Podcast URL</p>", unsafe_allow_html=True)   
-
-    url = st.sidebar.text_input("Castbox Podcast URL", placeholder="example: https://castbox.fm/channel/How-to-Talk-to-People-id4580807?country=us")
-    
-
-    process_button = st.sidebar.button("Process Podcast Feed")
-    st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
-
     if selected_podcast:
 
         podcast_info = available_podcast_info[selected_podcast]
@@ -111,71 +100,6 @@ def main():
         st.markdown(
                 f"</ul>", unsafe_allow_html=True)  
 
-    if process_button:
-
-        # Call the function to process the URLs and retrieve podcast guest information
-        podcast_info = process_podcast_info(url)
-        
-        # Display the podcast title
-        st.subheader(":orange[Episode Title]")
-        # st.write(podcast_info['podcast_details']['episode_title'].replace(".mp3",""))
-        st.markdown(
-                f"<p style='margin-bottom: 2px; font-size: 22px;'>{podcast_info['podcast_details']['episode_title'].replace('.mp3','')}</p>", unsafe_allow_html=True)   
-
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
-
-        with col1:
-            # Display the podcast episode summary
-            st.subheader(":orange[Podcast Episode Summary]")
-            st.write(podcast_info['podcast_summary'])
-
-        with col2:
-    
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover",  use_column_width=True)
-
-            # Episode Score By AI
-            
-            st.markdown(
-                    f"""<div style='background-color: #FF5F2A; padding: 11px; border-radius: 9px;'><h3 style='margin-bottom: 5px; color:#273346; text-align: center;'>Score By AI</h3>
-                    <p style='margin-bottom: 5px; text-align: center; font-size: 28px; color: #ffffff; font-weight: bold;'>{round(float(podcast_info['podcast_score']['score']),1)}/5.0</p>
-                    </div>""", unsafe_allow_html=True)
-            
-        # Display the podcast guest and their details 
-       
-        st.subheader(":orange[Episode Guest]")
-        st.write(podcast_info['podcast_guest'])
-
-    
-        st.subheader(":orange[Guest Informarion]")
-        # st.write(podcast_info["podcast_guest_details"]['Guest Job'])
-        guest_details = podcast_info["podcast_guest_details"]
-        for info in guest_details:
-
-            st.markdown(
-                f"<p style='margin-bottom: 2px;'>{info}: {guest_details[info]}</p>", unsafe_allow_html=True)   
-        
-
-         # Display the Episode Review
-        st.subheader(":orange[Review By AI]")
-        # st.write(podcast_info['podcast_score']['review'])
-        st.markdown(
-                f"<p style='background-color: #FF5F2A; padding: 11px; border-radius: 9px; font-weight: bold;'>{podcast_info['podcast_score']['review']}</p>", unsafe_allow_html=True)
-
-        # Display the four key moments
-        st.subheader(":orange[Key Moments]")
-        key_moments = podcast_info['podcast_highlights']
-
-        st.markdown(
-                f"<ul style='margin-bottom: 5px;>", unsafe_allow_html=True)
-        for moment in key_moments:
-
-            st.markdown(
-                f"<li style='margin-bottom: 5px;'>{key_moments[moment]}</li>", unsafe_allow_html=True)   
-
-        st.markdown(
-                f"</ul>", unsafe_allow_html=True)  
-
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
     data_dict = {}
@@ -189,11 +113,6 @@ def create_dict_from_json_files(folder_path):
             data_dict[podcast_name] = podcast_info
 
     return data_dict
-
-def process_podcast_info(url):
-    f = modal.Function.lookup("corise-podcast-project-full", "process_podcast")
-    output = f.call(url, '/content/Podcast/')
-    return output
 
 if __name__ == '__main__':
     main()
